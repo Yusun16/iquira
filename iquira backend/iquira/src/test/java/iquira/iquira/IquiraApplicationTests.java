a1a20662ad1785dd290ca07333e28f4c85a23a30
Yusun16 <yusunguairabryan@gmail.com>
2024-10-17 14:37:01 -0500

    backend

---
+++ iquira backend/iquira/src/test/java/iquira/iquira/IquiraApplicationTests.java
@@ -0,0 +1,13 @@
+package iquira.iquira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IquiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
